af5d00b52c579844333aac4ba35ac41e37d39312
hunputer <wlgns3855@gmail.com>
2023-05-12 00:32:49 +0900

    commit

--- src/main/java/com/example/api/controller/ChatController.java
+++ src/main/java/com/example/api/controller/ChatController.java
@@ -1,14 +1,18 @@
 package com.example.api.controller;
 
+import com.example.api.util.ChatClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ChatController {
 
-    @GetMapping("chatInput")
+    @GetMapping("InChat")
     public void chatInput(){
-
+        ChatClient client = new ChatClient();
+        client.start();
     }
+
+
 
 }
